6947e63b727b37793d409324483cad0cda1f2d27
Brett <brett@pulsetheworld.com>
2012-06-20 03:39:18 -0400

    form area mostly done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 /* Author:
-
+Brett Bergmann
 */
+
+$(function(){
+
+	var $email_input=$('#mce-EMAIL'),
+	$email_submit=$('#mc-embedded-subscribe'),
+	timer=null,
+	change_color=function(){
+		if($email_input.val()!=""){
+			$email_submit.css("background-color","#ee462b").attr("disabled",false);
+		}
+		else{
+			$email_submit.css("background-color","").attr("disabled","disabled");
+		}
+	}
+
+
+	$email_input.focus(function(){
+		timer=setInterval(change_color,333);
+	}).blur(function(){
+		clearInterval(timer);
+		change_color();
+	})
+
+
+
+
+});
 
 
 
