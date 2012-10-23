b55c73e45749d3690d563618606900d228e9e93d
Brett <brett@socketstudio.com>
2012-10-22 20:44:53 -0400

    added back in roots theme

---
+++ wp/wp-content/themes/roots/templates/content-single.php
@@ -0,0 +1,18 @@
+<?php while (have_posts()) : the_post(); ?>
+  <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+  </article>
+<?php endwhile; ?>
+
+
+<!-- <header>
+  <h1 class="entry-title"><?php //the_title(); ?></h1>
+  <?php //get_template_part('templates/entry-meta'); ?>
+</header>
+<div class="entry-content">
+  <?php //the_content(); ?>
+</div>
+<footer>
+  <?php //wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+  <?php //the_tags('<ul class="entry-tags"><li>','</li><li>','</li></ul>'); ?>
+</footer>
+<?php //comments_template('/templates/comments.php'); ?> -->
