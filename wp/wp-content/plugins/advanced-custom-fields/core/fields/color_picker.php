1a9d0593d865e536b0a135ade7e74f7d2405329b
Brett <brett@socketstudio.com>
2012-10-30 22:23:44 -0400

    working on footer

---
+++ wp/wp-content/plugins/advanced-custom-fields/core/fields/color_picker.php
@@ -0,0 +1,111 @@
+<?php
+
+class acf_Color_picker extends acf_Field
+{
+
+	/*--------------------------------------------------------------------------------------
+	*
+	*	Constructor
+	*
+	*	@author Elliot Condon
+	*	@since 1.0.0
+	*	@updated 2.2.0
+	* 
+	*-------------------------------------------------------------------------------------*/
+	
+	function __construct($parent)
+	{
+    	parent::__construct($parent);
+    	
+    	$this->name = 'color_picker';
+		$this->title = __("Color Picker",'acf');
+		
+   	}
+   	
+	
+	/*--------------------------------------------------------------------------------------
+	*
+	*	admin_print_scripts / admin_print_styles
+	*
+	*	@author Elliot Condon
+	*	@since 3.0.0
+	* 
+	*-------------------------------------------------------------------------------------*/
+	
+	function admin_print_scripts()
+	{
+		wp_enqueue_script(array(
+			'farbtastic'
+		));
+	}
+	
+	function admin_print_styles()
+	{
+		wp_enqueue_style(array(
+			'farbtastic'
+		));
+  
+	}
+	
+	
+	/*--------------------------------------------------------------------------------------
+	*
+	*	create_field
+	*
+	*	@author Elliot Condon
+	*	@since 2.0.5
+	*	@updated 2.2.0
+	* 
+	*-------------------------------------------------------------------------------------*/
+	
+	function create_field($field)
+	{		
+		// html
+		echo '<input type="text" value="' . $field['value'] . '" class="acf_color_picker" name="' . $field['name'] . '" id="' . $field['name'] . '" />';
+
+	}
+	
+	
+	/*--------------------------------------------------------------------------------------
+	*
+	*	create_options
+	*
+	*	@author Elliot Condon
+	*	@since 2.0.6
+	*	@updated 2.2.0
+	* 
+	*-------------------------------------------------------------------------------------*/
+	
+	function create_options($key, $field)
+	{
+		// vars
+		$defaults = array(
+			'default_value'	=>	'',
+		);
+		
+		$field = array_merge($defaults, $field);
+
+		
+		?>
+		<tr class="field_option field_option_<?php echo $this->name; ?>">
+			<td class="label">
+				<label><?php _e("Default Value",'acf'); ?></label>
+				<p class="description"><?php _e("eg: #ffffff",'acf'); ?></p>
+			</td>
+			<td>
+				<?php 
+				$this->parent->create_field(array(
+					'type'	=>	'text',
+					'name'	=>	'fields['.$key.'][default_value]',
+					'value'	=>	$field['default_value'],
+				));
+				?>
+			</td>
+		</tr>
+		<?php
+	}
+	
+	
+}
+
+?>
