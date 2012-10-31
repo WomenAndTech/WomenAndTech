7ab3e45936b72245c0426ff378e0e08da107b2d8
Brett <brett@socketstudio.com>
2012-10-31 19:13:02 -0400

    moved footer into wordpress

--- wp/wp-content/themes/roots/templates/teaser.php
+++ wp/wp-content/themes/roots/templates/teaser.php
@@ -1,4 +1,3 @@
-
 <?php
 
 if (is_home()||is_single()||is_frontpage()) $ids[]=$post->ID;
@@ -11,14 +10,17 @@ $loop = new WP_Query( $args ); ?>
 <?php if ($loop->have_posts()) : ?>
   <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-    <?php $teaser_image=get_field('teaser_image'); $image_url=$teaser_image['url'] ?>
+    <?php $teaser_image=get_field('teaser_image');
+          $prev_image_url=$teaser_image['url'];
+          $prev_url=get_permalink();
+    ?>
     
   <?php endwhile; ?>
 <?php else:
-  $image_url=get_template_directory_uri().'/assets/img/no-previous-teaser.jpg'; 
+  $prev_image_url=get_template_directory_uri().'/assets/img/no-previous-teaser.jpg';
+  $prev_url="#"; 
 endif; ?>
 
-<img src="<?php echo $image_url; ?>">
 
 
 <?php
@@ -30,8 +32,50 @@ $loop = new WP_Query( $args ); ?>
 
 <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-  <?php $teaser_image=get_field('teaser_image'); $image_url=$teaser_image['url'] ?>
+  <?php $teaser_image=get_field('teaser_image');
+        $next_image_url=$teaser_image['url'];
+        $next_url=get_permalink();
+  ?>
   
 <?php endwhile; ?>
 
-<img src="<?php echo $image_url; ?>">
+
+
+<div class="row" id="teasers">
+  <a href="<?php echo $prev_url; ?>">
+    <div class="span4">
+      <h3>Past Interviews</h3>
+      <div class="teaser-block">
+        <img src="<?php echo $prev_image_url; ?>">
+      </div>
+    </div>
+  </a>
+  <a href="<?php echo $next_url; ?>">
+    <div class="span4">
+      <h3>Up Next Time</h3>
+      <div class="teaser-block">
+        <img src="<?php echo $next_image_url; ?>">
+      </div>
+    </div>
+  </a>
+  <div class="span4">
+    <h3>Get Updates</h3>
+    <div class="teaser-block">
+      <div id="subscribe">
+        <form action="http://womenandtech.us5.list-manage.com/subscribe/post?u=1370c4a70b43a1285e806a56d&amp;id=123c00728e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+              <!-- Begin MailChimp Signup Form -->
+              <div class="mc-field-group">
+                <label for="mce-EMAIL">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-email-symbol.png"></label>
+
+                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
+              </div>
+              <div id="placeholder">
+                What's your email address?
+              </div>
+              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </form>
+        <!--End mc_embed_signup-->
+      </div>
+    </div>
+</div>
