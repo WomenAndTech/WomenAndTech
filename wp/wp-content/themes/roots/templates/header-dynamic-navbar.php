9ee47221be46d53cfd9ee985bc59e71db3ce1332
Brett <brett@socketstudio.com>
2012-11-02 01:20:10 -0400

    header, post, custom css

---
+++ wp/wp-content/themes/roots/templates/header-dynamic-navbar.php
@@ -0,0 +1,35 @@
+<header class="navbar" role="banner">
+  <div class="navbar-inner">
+    <div class="container-fluid">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+  <!-- /wp/wp-content/themes/roots/assets/ -->
+      <a class="brand" href="<?php echo home_url(); ?>/">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-womenandtech.png" alt="Women &amp;&amp; Tech">
+      </a>      
+      <div class="right">
+        <div class="contact">
+          <p class="follow">
+            <a href="https://twitter.com/intent/user?screen_name=womenandtech" target="_blank" rel='tooltip' title="@womenandtech">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bird_white_16.png">Follow Us!</a>
+          </p>
+          <p class="email">
+            <a href="#" title="contact@womenandtech.com"> <i class="icon-envelope icon-white"></i>
+              Sign up for updates
+            </a>
+          </p>
+        </div>
+        <nav id="nav-main" class="nav-collapse" role="navigation">
+          <?php
+            if (has_nav_menu('primary_navigation')) :
+              wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
+            endif;
+          ?>
+        </nav>
+      </div><!-- right -->
+    </div>
+  </div>
+</header>
