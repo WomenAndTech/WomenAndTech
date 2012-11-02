31fae57a3764cff1e52d62a268ca41f4b3352164
Brett <brett@socketstudio.com>
2012-11-01 23:28:21 -0400

    moving over header to wordpress

--- wp/wp-content/themes/roots/templates/byline.php
+++ wp/wp-content/themes/roots/templates/byline.php
@@ -14,7 +14,7 @@
       <div class="span3" id="contact">
         <p id="follow">
           <a href="https://twitter.com/intent/user?screen_name=womenandtech" target="_blank" rel='tooltip' title="@womenandtech">
-            <img src="https://dev.twitter.com/sites/default/files/images_documentation/bird_gray_16.png">Follow Us!</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bird_white_16.png">Follow Us!</a>
         </p>
         <p id="email">
           <a href="mailto:contact@womenandtech.com" rel="tooltip" title="contact@womenandtech.com"> <i class="icon-envelope icon-white"></i>
