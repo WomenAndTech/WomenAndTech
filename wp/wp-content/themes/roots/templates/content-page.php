c12ef9e59cdb87aa81de8b30261e665a91276775
Brett <brett@socketstudio.com>
2012-11-03 20:54:28 -0400

    updated base templates; lucia's interview moved in

--- wp/wp-content/themes/roots/templates/content-page.php
+++ wp/wp-content/themes/roots/templates/content-page.php
@@ -1,4 +1,20 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php 
+global $custom_content;
+  while (have_posts()) : the_post(); ?>
+
+<div class="<?php echo implode(get_post_class(), " ")." "; echo get_field('custom_css_class'); ?>
+  " id="post-
+  <?php the_ID(); ?>
+  ">
+  <?php if (!$custom_content): ?>
+  <div class="container">
+    <div class="row">
+      <div class="span12">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </div>
+  <?php else:
+          get_template_part("custom-content/$custom_content");
+      endif; ?></div>
 <?php endwhile; ?>
