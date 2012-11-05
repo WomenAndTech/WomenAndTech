f128e0282535bd7d12c18eda62631f90a9a3a4ff
Brett <brett@socketstudio.com>
2012-11-04 21:44:37 -0500

    modified content single for to display title in regular posts

--- wp/wp-content/themes/roots/templates/content-single.php
+++ wp/wp-content/themes/roots/templates/content-single.php
@@ -3,9 +3,16 @@ global $custom_content;
   while (have_posts()) : the_post(); ?>
     
     <article class="<?php echo implode(get_post_class(), " ")." "; echo get_field('custom_css_class'); ?>" id="post-<?php the_ID(); ?>">
-      <?php if (!$custom_content):   
-          the_content();
-        else:
+      <?php if (!$custom_content): ?>
+        <div class="container">
+          <div class="row">
+            <div class="span12">
+          <?php get_template_part('templates/page', 'header'); ?>
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </div>
+        <? else:
           get_template_part("custom-content/$custom_content");
       endif; ?>
     </article>
