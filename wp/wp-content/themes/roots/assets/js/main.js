3cf5789952f8816996512c24772cb3a968a7ed6e
Brett <brett@socketstudio.com>
2012-11-11 14:20:06 -0500

    working on scroll to subscribe area

--- wp/wp-content/themes/roots/assets/js/main.js
+++ wp/wp-content/themes/roots/assets/js/main.js
@@ -1,19 +1,34 @@
-/* Author:
-
-*/
-
 (function($){
 
-  $(document).on('click', 'a[href=#subscribe]', function(){
-    $flash = $('#subscribe').css('background', '#ee462b').closest('.span4').css('background', '#F68A77').end().find('input.email').focus().end();
-    setTimeout(function(){
-      $flash.css('background', '#dedddd').closest('.span4').css('background', '#f3f3f3');
-    },250)
+  $(document).on('click', 'a[href=#subscribe]', function(e){
+
+    /* Scrolling to the subscribe area */
+    e.preventDefault();
+    var $subscribe = $('#subscribe')
+    , subscribe_pos=$subscribe.offset().top
+    //Callback for when we're done scrolling    
+    , flash_color = function(){
+                        $subscribe.css('background', '#ee462b').closest('.span4').css('background', '#F68A77').end().find('input.email').focus().end();
+
+                          setTimeout(function(){
+                            $subscribe.css('background', '#dedddd').closest('.span4').css('background', '#f3f3f3');
+                          },250);
+                        }
+  
+    if($(window).scrollTop() + $(window).height() < subscribe_pos) {
+      $('html,body').stop().animate({ scrollTop: subscribe_pos},500, flash_color);
+    }
+    else{
+      flash_color()
+    }
+    return false;
   })
   .on('focus', 'input.email', function(){
+    /* Remove mock cursor in the subscribe email field */
     $(this).css('border-left', '0px');
   })
   .on('blur', 'input.email', function(){
+    /* Add back in mock cursor when other not typing anymore */
     $(this).css('border-left', '');
   })
 
