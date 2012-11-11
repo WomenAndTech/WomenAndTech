af78540bd1475716b771ddebd72c95ce4bb6c113
Brett <brett@socketstudio.com>
2012-11-10 21:21:42 -0500

    fixed error in content-single with php fenced code not runnig

--- wp/wp-content/themes/roots/templates/content-single.php
+++ wp/wp-content/themes/roots/templates/content-single.php
@@ -1,6 +1,6 @@
 <?php 
 global $custom_content;
-  while (have_posts()) : the_post(); ?>
+  while (have_posts()): the_post(); ?>
     
     <article class="<?php echo implode(get_post_class(), " ")." "; echo get_field('custom_css_class'); ?>" id="post-<?php the_ID(); ?>">
       <?php if (!$custom_content): ?>
@@ -12,7 +12,7 @@ global $custom_content;
         </div>
       </div>
     </div>
-        <? else:
+        <?php else:
           get_template_part("custom-content/$custom_content");
       endif; ?>
     </article>
