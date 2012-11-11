77c6eeaf935d1d30297e636c26ca123b8d8366cb
Brett <brett@socketstudio.com>
2012-11-11 17:02:34 -0500

    quick update to Rays amazing supporters page!

--- wp/wp-content/themes/roots/custom-content/supporters.php
+++ wp/wp-content/themes/roots/custom-content/supporters.php
@@ -1,5 +1,3 @@
-<?php require_once('header.php'); ?>
-
 <article id="sponsors" class="container">
 	<div class="row">
 		<div class="span12">
@@ -20,7 +18,7 @@
 			<div class="row">
 				<div id="sponsor_logo" class="span4">
 					<p>
-						<img src="img/supporters.png" alt="" title="" />
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/supporters/supporters.png" alt="" title="" />
 					</p>
 				</div>
 
@@ -42,25 +40,25 @@
 	<div class="row">
 		<div class="span3">
 			<a href="#" alt="People &amp; Code Inc." title="People &amp; Code Inc.">
-				<img src="img/peopleandcode.png" alt="People &amp; Code" title="People & Code Inc." />
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/supporters/peopleandcode.png" alt="People &amp; Code" title="People &amp; Code Inc." />
 			</a>
 			<p>The reason we got our team together in the first place was because these guys were ready to get building! Now both founders <a href="http://twitter.com/raykao" alt="Ray Kao" title="Ray Kao">Ray</a> and <a href="http://twitter.com/milang" alt="Milan Gokhale" title="Milan Gokhale">Milan</a> are on our team.</p>
 		</div>
 		<div class="span3">
 			<a href="http://mycitylives.com" alt="My City Lives" title="My City Lives">
-				<img src="img/mycitylives.png" alt="My City Lives" title="My City Lives" />
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/supporters/mycitylives.png" alt="My City Lives" title="My City Lives" />
 			</a>
 			<p>My City Lives offered up their offices for us to meet in without hesitation. <a href="http://twitter.com/amirad" alt="Amira Dhalla" title="Amira Dhalla">Amira Dhalla</a>, cofounder, is one of the coolest women in tech we know.</p>
 		</div>
 		<div class="span3">
 			<a href="http://http://www.ecentricarts.com/" alt="Ecentricarts" title="Ecentricarts">
-				<img src="img/ecentricarts.png" alt="Ecentricarts" title="Ecentricarts" />
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/supporters/ecentricarts.png" alt="Ecentricarts" title="Ecentricarts" />
 			</a>
 			<p>Our other headquarters is at ecentricarts, who have a high percentage of female developers. If that isn’t endorsement, we don’t know what is.</p>
 		</div>
 		<div class="span3">
 			<a href="http://www.starbucks.ca/" alt="Starbucks" title="Starbucks">
-				<img src="img/starbucks.png" alt="Starbucks" title="Starbucks" />
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/supporters/starbucks.png" alt="Starbucks" title="Starbucks" />
 			</a>
 			<p>Starbucks helps feed our ongoing caffeine habits and gives us lovely places to host our interviews across Toronto. Thanks <a href="http://twitter.com/StarbucksCanada" alt="Starbucks Canada - Vicki Laszlo" title="Starbucks Canada - Vicki Laszlo" >Vicki!</a></p>
 		</div>
@@ -76,5 +74,3 @@
 </div>
 
 </article>
-
-<?php require_once('footer.php'); ?>
