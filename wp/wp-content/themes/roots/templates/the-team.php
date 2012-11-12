e4ac5bc4eaac22094b0257fc13bfd3e966ecdfd9
Brett <bergmann.brett@gmail.com>
2012-11-12 09:11:30 -0500

    making the footer responsive

---
+++ wp/wp-content/themes/roots/templates/the-team.php
@@ -0,0 +1,24 @@
+<div class="span6" id="the-team">
+        <p><i class="icon-heart-empty icon-white"></i> Brought to you by
+        <span id='team-links'>
+          <a href="http://twitter.com/cassiemc" rel='tooltip' title="Cassie McDaniel" target="_blank" id='border-color1'>
+            <img src="http://api.twitter.com/1/users/profile_image/cassiemc">
+          </a>
+          <a href="http://twitter.com/britburger" target="_blank" rel='tooltip' title="Mark Staplehurst" id='border-color2'>
+              <img src="http://api.twitter.com/1/users/profile_image/britburger">
+          </a>
+          <a href="http://twitter.com/brttb" target="_blank" rel='tooltip' title="Brett Bergmann" id='border-color3'>
+            <img src="http://api.twitter.com/1/users/profile_image/brttb">
+          </a>
+          <a href="http://twitter.com/ivonnekn" target="_blank" rel='tooltip' title="Ivonne Karamoy" id='border-color4'>
+                       <img src="http://api.twitter.com/1/users/profile_image/ivonnekn">
+          </a>
+          <a href="http://twitter.com/milang" target="_blank" rel='tooltip' title="Milan Gokhale" id='border-color5'>
+            <img src="http://api.twitter.com/1/users/profile_image/milang">
+          </a>
+          <a href="http://twitter.com/RayKao" target="_blank" rel='tooltip' title="Ray Kao" id='border-color6'>
+             <img src="http://api.twitter.com/1/users/profile_image/RayKao">
+          </a>
+        </span>
+        </p>
+      </div>
