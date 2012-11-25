96a0571a63f6237eea6734e1dcb724ada6052bd8
Mark Staplehurst <britishburger@gmail.com>
2012-11-24 21:27:22 -0500

    Small CSS fixes, Compressed Images and cleanup

--- wp/wp-content/themes/roots/templates/header-dynamic-navbar.php
+++ wp/wp-content/themes/roots/templates/header-dynamic-navbar.php
@@ -1,39 +1,27 @@
 <header class="navbar navbar-fixed-top" role="banner">
   <div class="navbar-inner">
     <div class="container">
-    <a class="brand" href="<?php echo home_url(); ?>/">
+      <a class="brand" href="<?php echo home_url(); ?>/">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-womenandtech.png" alt="Women &amp;&amp; Tech">
-    </a>
+      </a>
       <div class="right">
         <div class="contact">
-         <p class="nomination-link">
-           <a href="#nomination-form" data-href="#nomination-form" data-toggle="modal" class="hidden-phone"> <i class="icon-pencil icon-white"></i>
-             Nominate
-           </a>
-           <a href="https://docs.google.com/spreadsheet/viewform?formkey=dDRqNXdEZElvMjItbDItbGhrZTdHRmc6MQ" class="visible-phone visible-table" target="_blank"><i class="icon-pencil icon-white"></i>
-           Nominate</a>
+          <p class="nomination-link">
+            <a href="#nomination-form" data-href="#nomination-form" data-toggle="modal" class="hidden-phone">
+              <i class="icon-pencil icon-white"></i> Nominate </a>
+            <a href="https://docs.google.com/spreadsheet/viewform?formkey=dDRqNXdEZElvMjItbDItbGhrZTdHRmc6MQ" class="visible-phone visible-table" target="_blank"><i class="icon-pencil icon-white"></i> Nominate</a>
           </p>
           <p class="follow">
             <a href="https://twitter.com/intent/user?screen_name=womenandtech" target="_blank" rel='tooltip' title="@womenandtech">
               <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bird_white_16.png">Follow Us!</a>
           </p>
-          <!--
-            // Issue on the interview page - needs to be resolved before we can comment the subscribe action back in
-          <p class="email">
-            <a href="#subscribe" title="Subscribe!"> <i class="icon-envelope icon-white"></i>
-              Sign up for updates
-            </a>
-          </p>
-          -->
         </div>
         <nav id="nav-main" role="navigation">
           <?php
             if (has_nav_menu('primary_navigation')) :
-              wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
-            endif;
-          ?>
+              wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav')); endif; ?>
         </nav>
-      </div><!-- right -->
+      </div>
     </div>
   </div>
 </header>
