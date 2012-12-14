903e6ae39baf11ff79d8ecf24f61bc14df60921e
Raymond Kao <ray@peopleandcode.com>
2012-12-13 19:47:56 -0500

    updated the pull-quote-tweet to adjust 140 char count and add interviewee name

--- wp/wp-content/themes/roots/assets/js/waat-pull-quote-tweet.js
+++ wp/wp-content/themes/roots/assets/js/waat-pull-quote-tweet.js
@@ -1,11 +1,25 @@
 jQuery(document).ready(function($){
 	pullQuote = $('.pull-quote aside.quote');
 	
-	address = parent.location ;
-
+	address = window.location.toString();
+	interviewee = $('header > div > h1').text();
+	
 	pullQuote.each(function(index){
-		text = $(this).find('h2').text();	
-		$(this).find('h2').append('<p><a href="https://twitter.com/share" class="twitter-share-button" data-url="' + address + '" data-text="tweet this" data-via="WomenAndTech" >Tweet</a></p>');
+		via = "WomenAndTech";
+		var quote = $(this).find('h2').text();
+
+		combinedText = interviewee + ": " + quote + " " + address + " via @" + via;
+
+		if (combinedText.length > 135) {
+			diff = Math.abs(combinedText.length - 135);
+			diff = Math.abs(quote.length - diff);
+			quote = quote.substr(0, diff);
+			quote = '&quot;' + quote + '...&quot;';
+		}
+
+		quote = interviewee + ": " + quote;
+
+		$(this).find('h2').append('<p><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://WomenAndTech.com" data-text="'+ quote + '" data-via="' + via + '" >Tweet</a></p>');
 	});
 
 	!function(d,s,id){
