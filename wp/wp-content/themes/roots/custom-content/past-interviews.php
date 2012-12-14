fff0169c0c0fd2ffb0d6624196134fb7ec3a0211
Brett <bergmann.brett@gmail.com>
2012-12-13 23:45:02 -0500

    refactored common styles for interviews in its own .css

--- wp/wp-content/themes/roots/custom-content/past-interviews.php
+++ wp/wp-content/themes/roots/custom-content/past-interviews.php
@@ -1,5 +1,5 @@
 <?php
-$args = array( 'post_type' => array('post','interview'), 'posts_per_page' => 999, 'post_status' => array('publish', 'future'), 'order' => 'ASC' );
+$args = array( 'post_type' => array('post','interview'), 'posts_per_page' => 999, 'post_status' => array('publish', 'future'), 'order' => 'DESC' );
 
 query_posts( $args ); ?>
 
