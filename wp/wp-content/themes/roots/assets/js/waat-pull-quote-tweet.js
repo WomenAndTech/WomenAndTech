7c2612abe905e5f15fc3f7fb51de906ab8fd3a23
Raymond Kao <ray@peopleandcode.com>
2012-12-17 16:53:44 -0500

    polised up js a bit more

--- wp/wp-content/themes/roots/assets/js/waat-pull-quote-tweet.js
+++ wp/wp-content/themes/roots/assets/js/waat-pull-quote-tweet.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function($){
 
-	var pullQuotes = $('.pull-quote aside.quote');
-	var pageURL = window.location.toString();
-	var intervieweeName = $('header > div > h1').text();
+	var pullQuotes = $('.pull-quote aside.quote'),
+			pageURL = window.location.toString(),
+			intervieweeName = $('header > div > h1').text();
 	
 	pullQuotes.each(function(index){
-		var viaHandle = "WomenAndTech";
-		var quoteText = $(this).find('h2').text();
-		var combinedText = intervieweeName + ": " + quoteText + " " + pageURL + " via @" + viaHandle;
+		var viaHandle = "WomenAndTech",
+				quoteText = $(this).find('h2').text(),
+				combinedText = intervieweeName + ": " + quoteText + " " + pageURL + " via @" + viaHandle;
 
 		if (combinedText.length > 135) {
 			var diff = Math.abs(combinedText.length - 135);
