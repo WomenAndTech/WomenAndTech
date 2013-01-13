07f5d5eceda0282378df80c6cdf9ed86bc363bdb
Brett <bergmann.brett@gmail.com>
2013-01-13 14:33:21 -0500

    removed related links

--- wp/wp-content/themes/roots/custom-content/ayla-interview.php
+++ wp/wp-content/themes/roots/custom-content/ayla-interview.php
@@ -109,7 +109,7 @@ She suggested that I apply to art school because she knew I wasn’t enjoying my
           </h3>
           <a name='art-in-the-family'></a>
           <p>
-            <a name="potter1" class="related-anchor"></a><span class="related potter"><a href="#potter2"><i class="icon-bolt"></i> My Nanna is a potter so there’s art in my family.</a></span>  My parents weren’t totally surprised. I struggled a lot as a teenager, being awkward and artsy in a town full of athletes and outdoor enthusiasts. I used to beg my parents to let me stay home from the ski hill, lock myself in my bedroom for a weekend and design board games.
+            My Nanna is a potter so there’s art in my family. My parents weren’t totally surprised. I struggled a lot as a teenager, being awkward and artsy in a town full of athletes and outdoor enthusiasts. I used to beg my parents to let me stay home from the ski hill, lock myself in my bedroom for a weekend and design board games.
           </p>
         </div>
       </section>
@@ -120,7 +120,7 @@ She suggested that I apply to art school because she knew I wasn’t enjoying my
             I started a book publishing company, which obviously was a crazy idea when you look at the technology around self-publishing today. I was the art director and designer and had partners who took care of the business aspects.
           </p>
           <p>
-            I was excited about the opportunity to be part of a cultural movement of some sort. <span class="related publishing"><a href="#"><i class="icon-bolt"></i> We were publishing art-related books, plays, poetry, short story anthologies.</a></span> It was small-scale, but local and inspiring. It gave people an opportunity to publish when they may not have otherwise. I liked that aspect of it.
+            I was excited about the opportunity to be part of a cultural movement of some sort. We were publishing art-related books, plays, poetry, short story anthologies. It was small-scale, but local and inspiring. It gave people an opportunity to publish when they may not have otherwise. I liked that aspect of it.
           </p>
         </div>
       </section>
@@ -150,8 +150,8 @@ the Institute without Boundaries, Bruce Mau Design and Pentagram.
         <div class="span7 offset4 interview-panel">
           <h3>Have you had any influential mentors?</h3>
           <p>
-            Kelsey Blackwell, a graphic designer and my mentor at Emily Carr. She was the first person who showed me that there is a kind of design that involved words, that writing is a relevant way to approach design challenges. She always pushed me to pursue bigger and better things.  <span class="related grad"><a href="#"><i class="icon-bolt"></i> She convinced me to apply for the
-            grad school that I went to in Toronto.</a></span>
+            Kelsey Blackwell, a graphic designer and my mentor at Emily Carr. She was the first person who showed me that there is a kind of design that involved words, that writing is a relevant way to approach design challenges. She always pushed me to pursue bigger and better things. She convinced me to apply for the
+            grad school that I went to in Toronto.
           </p>
         </div>
       </section>
@@ -214,7 +214,7 @@ the Institute without Boundaries, Bruce Mau Design and Pentagram.
         <div class="span7 offset4 interview-panel">
           <a name='asking-good-questions'></a>
           <p>
-            When I call myself a designer, I’m not thinking about myself as a graphic designer or even an interaction designer. I think ‘designer’ deserves a much broader definition. <a name='questions1' class='related-anchor'></a><span class="related questions"><a href="#questions2"> <i class="icon-bolt"></i> I think of a designer as someone who can ask good questions, </a></span>someone who really wants to understand a big picture perspective as well as the little details.
+            When I call myself a designer, I’m not thinking about myself as a graphic designer or even an interaction designer. I think ‘designer’ deserves a much broader definition. I think of a designer as someone who can ask good questions, someone who really wants to understand a big picture perspective as well as the little details.
           </p>
           <p>
             If you engage with a designer at the beginning of a process they can help you not only design the cover of a book, for instance, but also understand why you’re putting it out, and who it might go to, why it matters.
@@ -272,8 +272,8 @@ the Institute without Boundaries, Bruce Mau Design and Pentagram.
       <section class="row">
         <div class="span7 offset4 interview-panel">
           <p>
-            I recently visited the <span class="related school-of-life"><a href="#"><i class="icon-bolt"></i></a>School of Life
-              in London, UK</span>, which teaches classes on topics like, “How to Have Better Conversations” or “How to Make Love Last”. What they are developing at <a href="http://www.schooloflife.com">The School of Life</a> is a new kind of curriculum which is actually a new technology.
+            I recently visited the School of Life
+              in London, UK, which teaches classes on topics like, “How to Have Better Conversations” or “How to Make Love Last”. What they are developing at <a href="http://www.schooloflife.com">The School of Life</a> is a new kind of curriculum which is actually a new technology.
             </p>
           </div>
           <div class="span11 pull-quote">
@@ -287,8 +287,8 @@ the Institute without Boundaries, Bruce Mau Design and Pentagram.
           </div>
           <div class="span7 offset4 interview-panel">
             <p>
-              I'm interested in how design and technology can improve human wellness. I think we get confused between "technology" and digital tools like smartphones, tablets, QR codes, etc. If someone develops a new technology, it’s as if they’ve developed a new raw material. <a name="potter2" class="related-anchor"></a><span class="related potter"><a href="#potter1"><i class="icon-bolt"></i> It’s like a potter
-              discovering a new type of clay.</a></span> As a designer, I feel the same way. I think: <em>“I wonder what I can make with that!”</em>
+              I'm interested in how design and technology can improve human wellness. I think we get confused between "technology" and digital tools like smartphones, tablets, QR codes, etc. If someone develops a new technology, it’s as if they’ve developed a new raw material. It’s like a potter
+              discovering a new type of clay. As a designer, I feel the same way. I think: <em>“I wonder what I can make with that!”</em>
             </p>
           </div>
         </section>
@@ -366,9 +366,7 @@ the Institute without Boundaries, Bruce Mau Design and Pentagram.
             <p>
               Perspective. For example, I did a bunch of interviews when I first started working on this project and I talked to a guy who said he had spent 15,000 hours developing skills for his career. I asked him how long he’d spent developing his skills around relationships and he said,
               <em>“Maybe 15.”</em>
-              That’s amazing! We don’t spend time thinking about relationships as a skill. <a name='questions2' class='related-anchor'></a><span class="related questions"><a href="#questions1"><i class="icon-bolt"></i> We’re not
-              asking the right questions.</a></span>
-              To have better relationships we need to design them more intentionally.
+              That’s amazing! We don’t spend time thinking about relationships as a skill. We’re not asking the right questions. To have better relationships we need to design them more intentionally.
             </p>
           </div>
         </section>
@@ -378,9 +376,7 @@ the Institute without Boundaries, Bruce Mau Design and Pentagram.
               Would you consider designing relationships between people to be one of your life goals?
             </h3>
             <p>
-              I think certainly helping people develop emotional intelligence is a big goal for me. <span class="related school-of-life"><a href="#"><i class="star-empty
-star-empty
-icon-star"></i></a>Perhaps starting a school like the School of Life, that would be a major life accomplishment.</span>
+              I think certainly helping people develop emotional intelligence is a big goal for me. Perhaps starting a school like the School of Life, that would be a major life accomplishment.
             </p>
           </div>
         </section>
@@ -407,7 +403,7 @@ icon-star"></i></a>Perhaps starting a school like the School of Life, that would
             <h3>How did you get into tech in general?</h3>
             <a name='post-graduate-degree'></a>
             <p>
-               <span class="related grad"><a href="#"><i class="icon-bolt"></i> I did a post graduate degree in Toronto at the Institute without Boundaries in Interdisciplinary Design.</a></span> From there I was offered a job at a design studio in Toronto called Normative Design
+               I did a post graduate degree in Toronto at the Institute without Boundaries in Interdisciplinary Design. From there I was offered a job at a design studio in Toronto called Normative Design
               <http://normativedesign.com>
                 . That’s where I learned about how to design for the web, because before that I had only been taught print design. I wound up working with a lot of developers and learning their language.
               </p>
@@ -455,7 +451,7 @@ icon-star"></i></a>Perhaps starting a school like the School of Life, that would
               </p>
 
               <p>
-                I think what Women &amp;&amp; Tech is doing is important because you’re publishing ideas and interviews. Looking at art history, the groups that are remembered are the ones that <span class="related publishing"><a href="#"><i class="icon-bolt"></i></a>published and documented their work; that’s effective and important.</span>
+                I think what Women &amp;&amp; Tech is doing is important because you’re publishing ideas and interviews. Looking at art history, the groups that are remembered are the ones that published and documented their work; that’s effective and important.
               </p>
             </div>
           </section>
