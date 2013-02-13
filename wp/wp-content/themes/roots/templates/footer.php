f4767ad4fabbe588335cec4c69573ef8f54c422c
Raymond Kao <ray@peopleandcode.com>
2013-02-13 15:12:55 -0500

    added back google analytics tracking code

--- wp/wp-content/themes/roots/templates/footer.php
+++ wp/wp-content/themes/roots/templates/footer.php
@@ -10,13 +10,12 @@
 </footer>
 
 
-<?php if (GOOGLE_ANALYTICS_ID) : ?>
+
 <script>
-  var _gaq=[['_setAccount','<?php echo GOOGLE_ANALYTICS_ID; ?>'],['_trackPageview']];
+  var _gaq=[['_setAccount','UA-7465650-13'],['_trackPageview']];
   (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
     s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
-<?php endif; ?>
 
 <?php wp_footer(); ?>
