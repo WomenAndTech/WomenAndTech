7439d7687505e16e0e06bee26531e3c8e04dc58f
Brett <bergmann.brett@gmail.com>
2013-05-06 20:26:42 -0400

    updated pull quote tweet to work correctly and accept default data attributes

--- wp/wp-content/themes/roots/assets/js/pull-quote-tweet.js
+++ wp/wp-content/themes/roots/assets/js/pull-quote-tweet.js
@@ -3,15 +3,17 @@ jQuery(document).ready(function($){
 	var pullQuotes = $('.pull-quote aside.quote')
 		, permalink = $('body').data('permalink')
 		, urlEncodedPermalink = $('body').data('url-encoded-permalink')
-		, intervieweeName = $('article header h1').text()
+		, intervieweeName = $('body').data('interviewee') || $('article header h1').text().replace(/^\s+/,"")
 		, viaHandle = "WomenAndTech"
 		, maxLength = 117
 		;
 	
 	pullQuotes.each(function(index){
-		var quoteText = $(this).find('h2').text()
-			, combinedText = intervieweeName + ": " + quoteText + " via @" + viaHandle
+		var quoteText 
+			, combinedText
 			, tweetURL = "https://twitter.com/intent/tweet?"
+			, quoteText = $(this).data('tweet') || $(this).find('h2').text().replace(/^\s+/,"")
+			, combinedText = intervieweeName + ": " + quoteText + " via @" + viaHandle
 			;
 
 		if (combinedText.length > maxLength) {
@@ -21,13 +23,11 @@ jQuery(document).ready(function($){
 		}
 
 		quoteText = intervieweeName + ": " + quoteText;
-		tweetURL += 'text=' + quoteText;
+		tweetURL += 'text=' + encodeURIComponent(quoteText);
 		tweetURL += '&';
 		tweetURL += 'url=' + urlEncodedPermalink;
 		tweetURL += '&';
 		tweetURL += 'via=' + viaHandle;
-		
-		
 
 		$(this).find('h2').append('<a href="'+tweetURL+'" style="padding-left: 0.5em; border:none"><img src="https://dev.twitter.com/sites/default/files/images_documentation/bird_blue_16_1.png"></a>');
 	});
