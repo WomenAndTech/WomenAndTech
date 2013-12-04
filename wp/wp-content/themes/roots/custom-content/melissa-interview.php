cc952ef1db1a8f93c60def50db4d6942d74e03c1
Brett <bergmann.brett@gmail.com>
2013-12-03 22:08:17 -0500

    interview number css update

--- wp/wp-content/themes/roots/custom-content/melissa-interview.php
+++ wp/wp-content/themes/roots/custom-content/melissa-interview.php
@@ -3,7 +3,7 @@
   <div class="container interview-intro">
     <header class="row">
       <div class="span9">
-        <div class="interview-number double-digits"><?php echo get_field('interview_number')?></div>
+        <div class="interview-number double-digits interview-number-<?php echo get_field('interview_number') ?>"><?php echo get_field('interview_number')?></div>
         <h1 class="clearfix" id="twitter-handle" data-twitter-handle="">
           <?php the_title() ?></h1>
         <address>
