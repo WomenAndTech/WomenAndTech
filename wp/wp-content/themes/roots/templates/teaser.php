d333f8cc1f2753c2409332a3476189637ad76d6f
Erin Ward <erinxward@gmail.com>
2014-12-15 12:25:51 -0500

    footer

--- wp/wp-content/themes/roots/templates/teaser.php
+++ wp/wp-content/themes/roots/templates/teaser.php
@@ -68,7 +68,7 @@ $loop = new WP_Query( $args ); ?>
 <?php endwhile; ?>
 
 <?php
-  if (strpos($next_image_url,'interview') !== false) {
+  if (strpos($next_image_url,'interview') == false) {
     $next_image_text = "Contribute";
     $next_image_url = "assets/img/footer-contribute.jpg";
     $next_url = get_home_url()."/contributor-guidelines/";
